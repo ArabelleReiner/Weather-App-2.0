8a6fbc3b60407eb6d9e4b85007bbab96fdeab843
ArabelleReiner <102910087+ArabelleReiner@users.noreply.github.com>
2022-06-01 14:07:37 +0200

    Date4.0

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function formatDate(timestamp) {
   let currentHour = currentTime.getHours();
   let currentMinutes = currentTime.getMinutes();
   if (currentMinutes < 10) {
-    CurrentMinutes = `0${currentMinutes}`;
+    currentMinutes = `0${currentMinutes}`;
   }
 
   return `${currentDay}, ${currentDate}.${currentMonth}.${currentYear}, ${currentHour}:${currentMinutes}`;
